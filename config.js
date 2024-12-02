9f3a48f94b97cfb6c57d4729d71762f8c0efd8de
LordMaximon <fryopo@gmail.com>
2024-12-02 06:08:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || 'levanter_2b073cde72c0f442aa08bb10d6eb4d8e3').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_247ac6a38d2e947a5b2f4abf7cdcc0cf3',
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
